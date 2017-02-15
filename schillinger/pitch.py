dcdc347a52daa1c6d18bde26390339d7fa07969c
KoSv <k.svechtarov@gmail.com>
2017-02-15 23:10:17 +0100

    First commit

---
+++ schillinger/pitch.py
@@ -0,0 +1,47 @@
+'''
+    Schillinger System
+    Book II - Pitch
+    c. Konstantin Svechtarov 2017
+    
+'''
+import numpy as np
+
+
+class FirstGroup:
+    
+    def __init(self)__:
+        pass
+    
+    def one_unit_scales(self, tone):
+        return tone
+        pass
+    def two_unit_scales(self, tone):
+        scale_array = []
+        for i in range(12):
+            scale_array.append(tone+i%12)
+        return scale_array
+        pass
+    def three_unit_scales(self):
+        pass
+    def four_unit_scales(self):
+        pass
+    def seven_unit_scales(self):
+        pass
+    
+
+class SecondGroup:
+    
+    def __init()__:
+        pass
+    
+class ThirdGroup:
+    
+    def __init()__:
+        pass
+    
+    
+class FourthGroup:
+    
+    def __init()__:
+        pass
+    
